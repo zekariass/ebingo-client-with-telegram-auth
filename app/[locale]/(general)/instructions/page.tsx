b12012ac762e6fb62069bcd6a6723ef914402035
Zekarias Semegnew Negese <zekarias.s.negese@gmail.com>
2025-10-24 02:04:51 +0100

    add Oromifa and Tigrinya voice

--- app/[locale]/(general)/instructions/page.tsx
+++ app/[locale]/(general)/instructions/page.tsx
@@ -4,9 +4,12 @@ import React from "react"
 import { Button } from "@/components/ui/button"
 import { ArrowRightCircle, X } from "lucide-react"
 import { useRouter } from "next/navigation"
+import i18n from "@/i18n"
 
 export default function GameInstructions() {
   const router = useRouter()
+
+  i18n
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900 p-4">
--- bot/index.ts
+++ bot/index.ts
@@ -13,14 +13,14 @@ if (!process.env.APP_URL) throw new Error('APP_URL missing');
 
 export const bot = new Telegraf(process.env.BOT_TOKEN as string);
 
-// // install middleware and handlers
-// registerMiddleware(bot);
-// registerHandlers(bot);
+// install middleware and handlers
+registerMiddleware(bot);
+registerHandlers(bot);
 
-// // global error catcher
-// bot.catch((err) => console.error('Bot global error:', err));
+// global error catcher
+bot.catch((err) => console.error('Bot global error:', err));
 
-// // register commands & webhooks once
-// setCommandsAndWebhooks(bot).then(() => console.log('Bot setup complete')).catch(err => console.error('Setup error:', err));
+// register commands & webhooks once
+setCommandsAndWebhooks(bot).then(() => console.log('Bot setup complete')).catch(err => console.error('Setup error:', err));
 
 export default bot;
